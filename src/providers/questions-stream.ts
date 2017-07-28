83e6fe86eb291d61ecf8dfc1d841311605fe5b1e
valt <valt@valts-MBP.T-mobile.com>
2017-07-28 12:01:57 -0400

    minor refactoring/testing

--- src/providers/questions-stream.ts
+++ src/providers/questions-stream.ts
@@ -21,9 +21,8 @@ export class StreamData {
         }
 
     load(): any {
-
-        this.data = this.afDB.list('/userQuestions');
-        return this.data;
+      this.data = this.afDB.list('/userQuestions');
+      return this.data;
     }
 
     getUser(): any {
@@ -114,20 +113,30 @@ export class StreamData {
         return this.afDB.list('/answerViews');
     }
 
+    //Check if the answers are viewed by the user and if not mark as viewed
     updateViewedAnswers(question) {
-        //TODO Refactor
-        //Check if the answers are viewed by the user and if not mark as viewed
         this.views = this.fetchViewed();
         this.user = this.afAuth.auth.currentUser;
 
+        //If the user is logged in.
         if(this.user !== null) {
+
+            //if this is the users question
             if(question.userId === this.user.uid) {
+
+                //Grab all the answers for that question
                 this.http.get('https://einstein-981c4.firebaseio.com/userAnswers.json?orderBy="question_id"&equalTo="' + question.$key + '"').subscribe( (answers) => {
                 let objAnswers = answers.json();
+
+                //iterate through each question
                 Object.keys(objAnswers).forEach( (answer) => {
+
                     this.http.get('https://einstein-981c4.firebaseio.com/answerViews.json?orderBy="user_answer_id"&equalTo="' + this.user.uid + answer + '"').subscribe( (view) => {
+
+                    //check to see if the answer has been viewed already
                     if(Object.keys(view.json()).length === 0) {
-                        console.log('Message Viewed!')
+                        
+                        //if not, push a entry to the table labeling it as viewed.
                         this.views.push({ user_answer_id: this.user.uid + answer, value: true });
                     }
                     });
